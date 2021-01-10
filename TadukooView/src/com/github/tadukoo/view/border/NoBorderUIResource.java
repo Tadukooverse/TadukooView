b872a07cd44e2f1dc336d53cdd6f7c769902a276
Tadukoo <realtadukoo@gmail.com>
2021-01-09 22:06:43 -0500

    Fixed NoBorderUIResource
    - Issue #12
    - Needed to extend BorderUIResource instead of implementing UIResource (whoops)

--- TadukooView/src/com/github/tadukoo/view/border/NoBorderUIResource.java
+++ TadukooView/src/com/github/tadukoo/view/border/NoBorderUIResource.java
@@ -1,13 +1,21 @@
 package com.github.tadukoo.view.border;
 
-import javax.swing.plaf.UIResource;
+import javax.swing.plaf.BorderUIResource;
 
 /**
- * No Border UI Resource is a version of {@link NoBorder} that implements {@link UIResource}, so that it can be
+ * No Border UI Resource is a version of {@link NoBorder} that extends {@link BorderUIResource}, so that it can be
  * used in Look &amp; Feels in cases where you don't want a border, but can't use null
  *
  * @author Logan Ferree (Tadukoo)
  * @version Alpha v.0.3
  */
-public class NoBorderUIResource extends NoBorder implements UIResource{
+public class NoBorderUIResource extends BorderUIResource{
+	
+	/**
+	 * Creates a UIResource border object which wraps
+	 * a {@link NoBorder} instance.
+	 */
+	public NoBorderUIResource(){
+		super(new NoBorder());
+	}
 }
