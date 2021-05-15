3eff9aef3584e0750e878dfc84344bd5cd2874d4
Tadukoo <realtadukoo@gmail.com>
2021-05-15 17:37:28 -0400

    Added enums to be used for Tabbed Forms
    - Part of Issue #34

---
+++ TadukooForm/junit/com/github/tadukoo/view/form/tabbed/TabPlacementTest.java
@@ -0,0 +1,30 @@
+package com.github.tadukoo.view.form.tabbed;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.JTabbedPane;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TabPlacementTest{
+	
+	@Test
+	public void testTOP(){
+		assertEquals(JTabbedPane.TOP, TabPlacement.TOP.getValue());
+	}
+	
+	@Test
+	public void testBOTTOM(){
+		assertEquals(JTabbedPane.BOTTOM, TabPlacement.BOTTOM.getValue());
+	}
+	
+	@Test
+	public void testLEFT(){
+		assertEquals(JTabbedPane.LEFT, TabPlacement.LEFT.getValue());
+	}
+	
+	@Test
+	public void testRIGHT(){
+		assertEquals(JTabbedPane.RIGHT, TabPlacement.RIGHT.getValue());
+	}
+}
---
+++ TadukooForm/src/com/github/tadukoo/view/form/tabbed/TabLayoutPolicy.java
@@ -0,0 +1,35 @@
+package com.github.tadukoo.view.form.tabbed;
+
+import javax.swing.JTabbedPane;
+
+/**
+ * Tab Layout Policy represents what to do when there are too many tabs to fit in the given space on a Tabbed Form
+ *
+ * @author Logan Ferree (Tadukoo)
+ * @version Alpha v.0.3.3
+ */
+public enum TabLayoutPolicy{
+	/** The tabs will be wrapped if there are too many to fit in the given space */
+	WRAP_TAB_LAYOUT(JTabbedPane.WRAP_TAB_LAYOUT),
+	/** The tabs can be scrolled if there are too many to fit in the given space */
+	SCROLL_TAB_LAYOUT(JTabbedPane.SCROLL_TAB_LAYOUT);
+	
+	/** The int value used to represent the layout policy */
+	private final int value;
+	
+	/**
+	 * Constructs a new tab layout policy with the given value
+	 *
+	 * @param value The int value used to represent the layout policy
+	 */
+	TabLayoutPolicy(int value){
+		this.value = value;
+	}
+	
+	/**
+	 * @return The int value used to represent the layout policy
+	 */
+	public int getValue(){
+		return value;
+	}
+}
