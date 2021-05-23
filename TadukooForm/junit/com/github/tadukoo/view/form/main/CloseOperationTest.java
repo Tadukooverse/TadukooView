a33eef711a148f6139ac06377fc0c2f54363e0f8
Tadukoo <realtadukoo@gmail.com>
2021-05-22 21:01:04 -0400

    CloseOperation added
    - Part of Issue #36

---
+++ TadukooForm/junit/com/github/tadukoo/view/form/main/CloseOperationTest.java
@@ -0,0 +1,30 @@
+package com.github.tadukoo.view.form.main;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.JFrame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CloseOperationTest{
+	
+	@Test
+	public void testDO_NOTHING_ON_CLOSE(){
+		assertEquals(JFrame.DO_NOTHING_ON_CLOSE, CloseOperation.DO_NOTHING_ON_CLOSE.getValue());
+	}
+	
+	@Test
+	public void testHIDE_ON_CLOSE(){
+		assertEquals(JFrame.HIDE_ON_CLOSE, CloseOperation.HIDE_ON_CLOSE.getValue());
+	}
+	
+	@Test
+	public void testDISPOSE_ON_CLOSE(){
+		assertEquals(JFrame.DISPOSE_ON_CLOSE, CloseOperation.DISPOSE_ON_CLOSE.getValue());
+	}
+	
+	@Test
+	public void testEXIT_ON_CLOSE(){
+		assertEquals(JFrame.EXIT_ON_CLOSE, CloseOperation.EXIT_ON_CLOSE.getValue());
+	}
+}
