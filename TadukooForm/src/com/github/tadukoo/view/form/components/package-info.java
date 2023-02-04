7ce6a83e0781af1a8576cc3f3888db9714d26354
Tadukoo <realtadukoo@gmail.com>
2023-02-03 22:28:07 -0500

    Removed Tadukoo Form (now in its own project)
    - Issue #53

--- TadukooForm/src/com/github/tadukoo/view/form/components/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains special components that are made up via {@link com.github.tadukoo.view.form.Form Forms}
- */
-package com.github.tadukoo.view.form.components;
--- TadukooForm/src/com/github/tadukoo/view/form/field/number/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains {@link com.github.tadukoo.view.form.field.number.NumberFormField NumberFormField} and its subclasses.
- * These use {@link javax.swing.JSpinner JSpinner} for numeric
- * {@link com.github.tadukoo.view.form.field.FormField FormFields}.
- */
-package com.github.tadukoo.view.form.field.number;
